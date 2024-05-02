59622801a7dfb869c5493ead70c6c3be242bdf20
ritaOmondi <cherylritaomondi@gmail.com>
2024-05-02 04:15:35 -0700

    actualizing server ips

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["52.91.144.190", "54.175.133.200"]
 env.user = "ubuntu"
 
 
